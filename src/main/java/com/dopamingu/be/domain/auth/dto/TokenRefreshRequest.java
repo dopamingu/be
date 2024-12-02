da091019ad383a31970c1d09aae9f10c7059df87
OKSUchun <oksuchun119@gmail.com>
2024-12-02 16:49:36 +0900

    [feat] issue refresh token

---
+++ src/main/java/com/dopamingu/be/domain/auth/dto/TokenRefreshRequest.java
@@ -0,0 +1,14 @@
+package com.dopamingu.be.domain.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TokenRefreshRequest {
+    private String refreshToken;
+}
