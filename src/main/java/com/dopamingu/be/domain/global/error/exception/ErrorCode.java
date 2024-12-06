8dbf55874cbe41c238f9084d81ff68323423c2e1
OKSUchun <oksuchun119@gmail.com>
2024-12-06 20:54:45 +0900

    [fix] change DUPLICATE_USERNAME to 409 CONFLICT

--- src/main/java/com/dopamingu/be/domain/global/error/exception/ErrorCode.java
+++ src/main/java/com/dopamingu/be/domain/global/error/exception/ErrorCode.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     SAMPLE_ERROR(HttpStatus.BAD_REQUEST, "DP4000", "Sample Error Message"),
     NOT_EXISTING_OAUTH_PROVIDER(HttpStatus.BAD_REQUEST, "DP4001", "존재하지 않는 소셜로그인 플랫폼입니다."),
-    DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "DP4002", "중복된 닉네임입니다."),
     PREVIOUS_USERNAME(HttpStatus.BAD_REQUEST, "DP4003", "동일한 닉네임입니다."),
 
     EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "DP4010", "만료된 JWT 토큰입니다."),
@@ -19,7 +18,11 @@ public enum ErrorCode {
     MISSING_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "DP4014", "토큰 정보가 존재하지 않습니다."),
 
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "DP4040", "해당 회원을 찾을 수 없습니다."),
+
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "DP4050", "잘못된 HTTP 메서드입니다."),
+
+    DUPLICATE_USERNAME(HttpStatus.CONFLICT, "DP4090", "중복된 닉네임입니다."),
+
     UNPROCESSABLE_ENTITY(HttpStatus.UNPROCESSABLE_ENTITY, "DP4220", "요청 데이터 형식이 올바르지 않습니다."),
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DP500", "서버 에러가 발생했습니다.");
